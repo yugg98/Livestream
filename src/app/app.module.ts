407ea704d7d98342eaa043ceeb1b5b707182ba2a
yugg98 <70101980+yugg98@users.noreply.github.com>
2022-01-23 13:56:51 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LivestreamComponent } from './components/livestream/livestream.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
+    LivestreamComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
